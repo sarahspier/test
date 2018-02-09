7cefd039abe7bd351f0e034b9fd1de6b2ea5e4f2
Sarah Spier <sspier2@unl.edu>
2018-02-09 09:39:00 -0600

    Modified, added a category to the data - catfish length column added to the spreadsheet.

--- FishAnalysis.R
+++ FishAnalysis.R
@@ -2,4 +2,11 @@
 install.packages("tidyverse")
 library(tidyverse)
 
+fish_data <- read.csv("Gaeta_etal_CLC_data.csv")
 fish_data
+head(fish_data)
+
+#making a new coloumn; telling the column to name catfish big if their length exceeds 200 and small if not
+fish_data %>%
+  mutate(length_cat = ifelse(length>200, "big", "small")) -> fish_data_cat
+fish_data_cat
